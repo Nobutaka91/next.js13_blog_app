2fd57840921b0120368c8e752aff757af56e0b12
N <hatarakuotamasan8@outlook.jp>
2023-09-04 07:39:00 +0900

    frontendの実装が完了した

--- next.js13-tailwindcss-first-blog/src/app/page.tsx
+++ next.js13-tailwindcss-first-blog/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </section>
 
       <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
-        <div className="bg-white shadow-md rounded p-4 mb-6 mt-4">
+        <div className="bg-white shadow-md rounded p-4 mb-6 mt-4 w-full">
           <h3 className="font-bold text-gray-900 mb-2">About Me</h3>
           <p className="text-gray-600">
             Able to speak English can open the door to other possibilities, I

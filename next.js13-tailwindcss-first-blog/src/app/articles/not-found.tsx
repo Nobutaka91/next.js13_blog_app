925c480fc3ee5f506d8fcb5bf768af3df3664f2f
N <hatarakuotamasan8@outlook.jp>
2023-09-05 01:09:24 +0900

    create 404notFound page

---
+++ next.js13-tailwindcss-first-blog/src/app/articles/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center">
+        <h1 className="text-gray-800 text-2xl font-bold mb-4">404</h1>
+        <p className="text-gray-600">ページが見つかりませんでした。。。😭</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
